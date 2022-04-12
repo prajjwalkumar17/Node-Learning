5cee50ef555df1c5d1eb1e2e665afc3444d72eee
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-12 18:09:26 +0530

    Completed the node farm project

--- index.js
+++ index.js
@@ -10,6 +10,32 @@ const url = require("url");
 // fs.writeFileSync("./1-node-farm/output.txt", textOut);
 // console.log("File Written");
 ///////////////////////////Servers//////////////////////////////////
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+const templateOverview = fs.readFileSync(
+  "./1-node-farm/starter/templates/template-overview.html",
+  "utf-8"
+);
+const templateproduct = fs.readFileSync(
+  "./1-node-farm/starter/templates/template-product.html",
+  "utf-8"
+);
+const templatecard = fs.readFileSync(
+  "./1-node-farm/starter/templates/teplate-card.html",
+  "utf-8"
+);
 const data = fs.readFileSync(
   "./1-node-farm/starter/dev-data/data.json",
   "utf-8"
@@ -17,15 +43,38 @@ const data = fs.readFileSync(
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathname = req.url;
+  const { query, pathname } = url.parse(req.url, true);
+
+  //INIT overview Page
   if (pathname === "/" || pathname === "/overview") {
-    res.end("This is overview");
-  } else if (pathname === "/product") {
-    res.end("Hellow from the product");
-  } else if (pathname === "/api") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(templatecard, el))
+      .join("");
+    const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+  }
+
+  //INIT products page
+  else if (pathname === "/product") {
+    const product = dataObj[query.id];
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+    const output = replaceTemplate(templateproduct, product);
+    res.end(output);
+  }
+
+  //INIT API
+  else if (pathname === "/api") {
     res.writeHead(200, { "content-type": "application/JSON" });
     res.end(data);
-  } else {
+  }
+
+  //INIT notfound
+  else {
     res.writeHead(404, {
       "content-type": "text/HTML",
       myheader: "hehehehhehehhehehehehehheheh",
